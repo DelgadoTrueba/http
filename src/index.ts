a3b340787dadbddc4d805eff549839898f2f22a7
Delgado Trueba <jose.delgado@babel.es>
2024-08-15 15:52:13 +0200

    refactor: :recycle: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 export { HttpInterfaceService } from './lib/Http.interface.service';
 export { HttpService } from './lib/Http.service';
 export { HttpMockService } from './lib/HttpMock.service';
+
+export {
+  HttpRequest,
+  HttpGetRequest,
+  HttpPostRequest,
+  HttpPutRequest,
+  HttpPatchRequest,
+  HttpDeleteRequest,
+} from './lib/model/request/HttpRequest.model';
+
+export * from './lib/model/response/HttpResponse.model';
+export * from './lib/model/response/HttpStatusCodeResponse.model';
+export * from './lib/model/response/HttpResponseError.model';
--- src/public-api.ts
+++ src/public-api.ts
@@ -3,5 +3,6 @@ export * from './lib/model/request/HttpRequest.model';
 
 export * from './lib/model/response/HttpResponse.model';
 export * from './lib/model/response/HttpStatusCodeResponse.model';
+export * from './lib/model/response/HttpResponseError.model';
 
 export * from './lib/Http.interface.service';
